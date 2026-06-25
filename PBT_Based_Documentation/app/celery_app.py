aa6321aee1529d8da8f5ba7c2ce2c75e68f1a621
CatFatOw <michaelwufluffy@gmail.com>
2026-06-25 11:43:52 -0400

    adding render deploy fixes

--- PBT_Based_Documentation/app/celery_app.py
+++ PBT_Based_Documentation/app/celery_app.py
@@ -31,5 +31,5 @@ celery_app.conf.update(
 # Import tasks here. Support both `PYTHONPATH=app celery -A celery_app...`
 # and package-style `celery -A app.celery_app...` worker launches.
 
-from .tasks import export_tasks
-from .tasks import metric_task
+from app.tasks import export_tasks
+from app.tasks import metric_task
